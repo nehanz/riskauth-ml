b0a29d72dd043d19165a2bc108e4072143096955
Nehan Wijayagunarathna <nehannimsara0031029@gmail.com>
2026-02-05 22:41:39 +0530

    refactor: relocate files

--- inference/scorer.py
+++
@@ -1,6 +0,0 @@
-from features.extractor import extract_features
-from models.model import predict
-
-def score(event: dict) -> float:
-    features = extract_features(event)
-    return predict(features)
---
+++ riskauth_ml/inference/scorer.py
@@ -0,0 +1,6 @@
+from riskauth_ml.features.extractor import extract_features
+from riskauth_ml.models.model import predict
+
+def score(event: dict) -> float:
+    features = extract_features(event)
+    return predict(features)
